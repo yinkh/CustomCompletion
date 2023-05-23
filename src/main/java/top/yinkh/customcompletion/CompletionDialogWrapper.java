f122b2a1a00e250b41c22b64704f459973226ed9
yinkanghong <yinkanghong@xiaomi.com>
2023-05-23 09:43:44 +0800

    keywords输入框支持滚动

--- src/main/java/top/yinkh/customcompletion/CompletionDialogWrapper.java
+++ src/main/java/top/yinkh/customcompletion/CompletionDialogWrapper.java
@@ -2,15 +2,18 @@ package top.yinkh.customcompletion;
 
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.ui.components.JBScrollPane;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,7 +24,7 @@ import java.util.List;
  */
 
 public class CompletionDialogWrapper extends DialogWrapper {
-    private JTextField text;
+    private JTextArea text;
 
     public CompletionDialogWrapper() {
         super(true);
@@ -37,11 +40,20 @@ public class CompletionDialogWrapper extends DialogWrapper {
         label.setText("split by ';' eg: apple;orange");
         dialogPanel.add(label, BorderLayout.NORTH);
 
-        String customCompletion = PropertiesComponent.getInstance().getValue("custom_completion");
-        text = new JTextField();
-        text.setText(customCompletion);
-        text.setPreferredSize(new Dimension(500, 250));
-        dialogPanel.add(text, BorderLayout.CENTER);
+        List<String> completionList = PropertiesComponent.getInstance().getList("custom_completion");
+        text = new JTextArea(18,100);
+        // 自动换行
+        text.setLineWrap(true);
+        if (CollectionUtils.isEmpty(completionList)) {
+            text.setText("");
+        } else {
+            text.setText(StringUtils.join(completionList, ";"));
+        }
+
+        JBScrollPane areaScrollPane = new JBScrollPane(text);
+        areaScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        areaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        dialogPanel.add(areaScrollPane, BorderLayout.CENTER);
 
         return dialogPanel;
     }
@@ -52,6 +64,8 @@ public class CompletionDialogWrapper extends DialogWrapper {
             if (StringUtils.isNotEmpty(text.getText())) {
                 List<String> completionList = Arrays.stream(text.getText().split(";")).toList();
                 PropertiesComponent.getInstance().setList("custom_completion", completionList);
+            } else {
+                PropertiesComponent.getInstance().setList("custom_completion", new ArrayList<>());
             }
             close(OK_EXIT_CODE);
         }
