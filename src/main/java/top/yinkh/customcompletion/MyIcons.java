cbd7cd7f88147a58f5fe9429c4b84afc198a4cd1
yinkanghong <yinkanghong@xiaomi.com>
2023-05-24 13:38:05 +0800

    add icon

---
+++ src/main/java/top/yinkh/customcompletion/MyIcons.java
@@ -0,0 +1,15 @@
+package top.yinkh.customcompletion;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.Icon;
+
+/**
+ * @author yinkanghong
+ * @date 2023-05-24
+ * @desc
+ */
+
+public interface MyIcons {
+    Icon Action = IconLoader.getIcon("/icon/action.svg", MyIcons.class);
+}
